14b0dfc6253f93b04f0b459def0ef490d5f24b34
Faizan Ali <faizanali.mentorsol@gmail.com>
2025-02-16 23:13:26 +0500

    call api data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
 
 const Home = () => {
+  const fetchData = async () => {
+    const apiResponse = await fetch('https://api.spacexdata.com/v3/landpads');
+    const getData = await apiResponse.json();
+    return getData;
+  }
+  fetchData().then(data => console.log(data));
   return (
     <div className="text-[red] flex inset-y-1/4 ">Home</div>
   )
