1bfb11d3d59f09abefe7c81a58cfe013c3ff28b1
Faizan Ali <faizanali.mentorsol@gmail.com>
2025-02-21 14:12:40 +0500

    set some route name

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,15 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Blog from './pages/Blog';
-import Gallery from './pages/Gallery';
+import Services from './pages/Services';
 
 const App: React.FC = () => {
   const routeConfig = [
     { path: "/", element: <Home /> },
+    { path: "/Services", element: <Services /> },
     { path: "/About", element: <About /> },
     { path: "/Contact", element: <Contact /> },
     { path: "/PackageDetails", element: <Blog /> },
-    { path: "/Services", element: <Gallery /> }
   ];
   return (
     <>
