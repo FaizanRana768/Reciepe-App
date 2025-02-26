0227e925fbd3f45383a9998f0b2b3e5af0023de4
Faizan Ali <faizanali.mentorsol@gmail.com>
2025-02-26 18:22:02 +0500

    currently comment the footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -32,7 +32,7 @@ const App: React.FC = () => {
           ))}
         </Routes>
       </Box>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
