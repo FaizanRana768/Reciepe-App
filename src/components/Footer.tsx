cae1edb4ee8ba52903185ee51b597c25a26dc1c1
Faizan Ali <faizanali.mentorsol@gmail.com>
2025-02-26 18:03:00 +0500

    set the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,92 @@
-import { Box } from '@mui/material';
-import * as styles from '../styles/Footer.styles';
+import { Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
+import { contactDetails, services, socialLinks } from "../utils/constant";
 
-const Footer = () => {
+function Footer() {
   return (
-    <Box sx={styles.FooterStyle}>Footer</Box>
-  )
+    <footer className="bg-black text-gray-300 py-20 font-sans tracking-wide">
+      <div className="container mx-auto px-6 lg:px-16">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-16">
+          <div className="flex flex-col items-center text-center">
+            <Link to="/" className="inline-block">
+              <img src="/src/assets/images/Xsuitelogo.webp" alt="logo" className="w-48" />
+            </Link>
+            <p className="mt-4 text-sm">
+              Discover XSuite, a global marketing agency that transforms your brand with creativity and innovation.
+            </p>
+            <form className="mt-6 flex flex-col sm:flex-row items-center space-y-3 sm:space-y-0 sm:space-x-3">
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="px-4 py-2 w-full sm:w-auto rounded-md text-black focus:outline-none"
+                required
+              />
+              <button
+                type="submit"
+                className="px-4 py-2 rounded-md font-semibold transition duration-300 ease-in-out bg-white text-black hover:bg-orange-600 hover:text-white"
+              >
+                Subscribe
+              </button>
+            </form>
+          </div>
+
+          <div className="text-center lg:text-left">
+            <h4 className="text-white font-semibold text-lg">Get In Touch</h4>
+            <ul className="mt-6 space-y-2 text-gray-300">
+              {contactDetails.map((item, index) => (
+                <li key={index}>
+                  <span className="text-orange-500 font-bold">{item.type}:</span>{" "}
+                  {item.link ? (
+                    <Link to={item.link} target="_blank" className="hover:text-white">
+                      {item.value}
+                    </Link>
+                  ) : (
+                    item.value
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="text-center lg:text-left">
+            <h4 className="text-white font-semibold text-lg">Services</h4>
+            <ul className="mt-6 space-y-1">
+              {services.map((service, index) => (
+                <li key={index}>
+                  <Link to={service.path} className="hover:text-white">
+                    {service.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="text-center">
+            <h4 className="text-white font-semibold text-lg">Follow Us</h4>
+            <ul className="mt-6 space-y-4">
+              {socialLinks.map((social, index) => (
+                <li key={index} className="flex justify-center lg:justify-start items-center space-x-2">
+                  <Icon icon={social.icon} className="h-6 w-6 text-orange-500 duration-300 ease-in-out" />
+                  <Link to={social.url} target="_blank" className="hover:text-white">
+                    {social.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <hr className="my-10 border-gray-600" />
+
+        <div className="flex justify-center space-x-4 text-sm">
+          {["Terms of Service", "Privacy Policy", "Security"].map((text, index) => (
+            <Link key={index} to={`/${text.replace(/\s+/g, "")}`} className="hover:text-white">
+              {text}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </footer >
+  );
 }
 
-export default Footer
+export default Footer;
---
+++ src/utils/constant.ts
@@ -0,0 +1,24 @@
+export const services = [
+    { name: "SEO", path: "/Services/seo" },
+    { name: "Google Ads", path: "/Services/google-my-business-optimization" },
+    { name: "PPC Campaigns", path: "/Services/ppc-campaign" },
+    { name: "Google Analytics", path: "/Services/google-analytics" },
+    { name: "Digital Marketing", path: "/Services/digital-marketing" },
+    { name: "Graphic Designing", path: "/Services/google-my-business-optimization" },
+    { name: "Website Development", path: "/Services/website-development" },
+    { name: "Google Search Console", path: "/Services/google-search-console" },
+    { name: "Social Media Marketing", path: "/Services/social-media-marketing" }
+];
+
+export const socialLinks = [
+    { name: "Facebook", icon: "ic:baseline-facebook", url: "https://www.facebook.com/profile.php?id=61566739650180" },
+    { name: "Instagram", icon: "mdi:instagram", url: "https://www.instagram.com/xsuite.digital/" },
+    { name: "LinkedIn", icon: "mdi:linkedin", url: "https://www.linkedin.com/company/xsuite-digital/about/" }
+];
+
+export const contactDetails = [
+    { type: "Email", value: "info@xsuite.digital", link: "mailto:info@xsuite.digital" },
+    { type: "Phone", value: "+92 331 63 61 916", link: "https://wa.me/923064312116" },
+    { type: "Timing", value: "Mon-Sat: 9am-5pm" },
+    { type: "Location", value: "Lawrence Road Lahore, Pakistan", link: "https://www.google.com/maps?q=Lawrence+Road" },
+]
