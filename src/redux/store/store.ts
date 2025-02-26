fb5f23653b1491251ae55c27ded3de24681549a9
Faizan Ali <faizanali.mentorsol@gmail.com>
2025-02-26 17:07:44 +0500

    make counter slice

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import { counterSlice } from "../slices/Counter.slice";
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        counter: counterSlice.reducer,
+    },
 })
 
 export type RootState = ReturnType<typeof store.getState>
